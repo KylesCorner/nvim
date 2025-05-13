c69a3500779f80f312907344139e2a0ee9c32830
KylesCorner <kylekrstulich@gmail.com>
2025-05-13 11:26:32 -0600

    better code block running

--- lua/custom/plugins/jupyter_notebook.lua
+++ lua/custom/plugins/jupyter_notebook.lua
@@ -27,6 +27,47 @@ Then restart Neovim and run:
 :UpdateRemotePlugins
 And restart Neovim again.
 --]]
+
+-- Run Python code between triple backticks with Magma
+function RunMagmaBlock()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local line_nr = cursor[1]
+
+  local start_line = nil
+  local end_line = nil
+
+  -- Search upward for first ```
+  for i = line_nr, 1, -1 do
+    local line = vim.api.nvim_buf_get_lines(bufnr, i - 1, i, false)[1]
+    if line:match '^```python' then
+      start_line = i
+      break
+    end
+  end
+
+  -- Search downward for closing ```
+  local total_lines = vim.api.nvim_buf_line_count(bufnr)
+  for i = line_nr + 1, total_lines do
+    local line = vim.api.nvim_buf_get_lines(bufnr, i - 1, i, false)[1]
+    if line:match '^```' then
+      end_line = i
+      break
+    end
+  end
+
+  -- Make sure the block is valid and exclude the backtick lines
+  if start_line and end_line and (start_line + 1) < end_line then
+    -- Select between the backticks
+    vim.api.nvim_win_set_cursor(0, { start_line + 1, 0 })
+    vim.cmd 'normal! V'
+    vim.api.nvim_win_set_cursor(0, { end_line - 1, 0 })
+    vim.cmd 'MagmaEvaluateVisual'
+  else
+    print 'Could not find valid code block between triple backticks.'
+  end
+end
+
 return {
   {
     --backend
@@ -35,13 +76,16 @@ return {
     config = function()
       vim.cmd 'let g:magma_automatically_open_output = v:true'
       vim.cmd 'let g:magma_output_window_borders= v:true'
-      vim.keymap.set('n', '<localleader>mo', ':MagmaEvaluateOperator<CR>', { silent = true })
+      vim.cmd 'let g:magma_image_provider="kitty"'
+
+      vim.keymap.set('n', '<localleader>mp', ':MagmaEvaluateOperator<CR>', { silent = true })
       vim.keymap.set('n', '<localleader>ml', ':MagmaEvaluateLine<CR>', { silent = true })
       vim.keymap.set('x', '<localleader>mv', ':<C-u>MagmaEvaluateVisual<CR>', { silent = true })
       vim.keymap.set('n', '<localleader>mc', ':MagmaReevaluateCell<CR>', { silent = true })
       vim.keymap.set('n', '<localleader>md', ':MagmaDelete<CR>', { silent = true })
       vim.keymap.set('n', '<localleader>mo', ':MagmaShowOutput<CR>', { silent = true })
       vim.keymap.set('n', '<localleader>mi', ':MagmaInit<CR>', { silent = true })
+      vim.keymap.set('n', '<localleader>mb', RunMagmaBlock, { desc = 'Run magma block between backticks.' })
     end,
     ft = { 'python', 'julia', 'markdown' },
   },
